4bedd54cee254918ac4a3181684d91a0250d28d8
Denny Wiseman <d.wiseman@utoronto.ca>
2019-02-16 16:55:54 -0500

    set up flux, react, landing page, basic app structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './css/App.css'
+import Landing from './landing/Landing.js'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Landing />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/landing/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import SearchBar from './SearchBar.js'
+import SignInButton from './SignInButton.js'
+import '../css/landing/Landing.css'
+
+export default class Landing extends React.Component {
+  render () {
+    return (
+      <div className='landing'>
+        <div className='signInButtonContainer'>
+          <SignInButton />
+        </div>
+        <div className='landingCenterPanel'>
+          <h1>Where to?</h1>
+          <SearchBar />
+        </div>
+      </div>
+    )
+  }
+}

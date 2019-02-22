8bd01815bb1268dcefa88499b24ab79d4d0ba667
Denny Wiseman <d.wiseman@utoronto.ca>
2019-02-21 22:43:13 -0500

    signin button changes text based on loginstatus

--- src/components/landing/SignInButton.js
+++ src/components/landing/SignInButton.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import LoginStates from '../../LoginStates'
+import AppStore from '../AppStore'
 // import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
 export default class SignInButton extends React.Component {
+  constructor () {
+    super()
+    this.state = { buttonText: 'Sign in' }
+  }
+
+  componentDidMount () {
+    AppStore.on('change', this.updateState)
+  }
+
+  componentWillUnmount () {
+    AppStore.removeListener('change', this.updateState)
+  }
+
+  getButtonText = (loginStatus) => {
+    return loginStatus === LoginStates.loggedIn
+      ? 'Sign out' : 'Sign in'
+  }
+
+  updateState = () => this.setState({ buttonText: this.getButtonText(AppStore.getState().loggedInState) })
   render () {
     return (
       <Button variant='contained' color='primary' className='signInButton'
         onClick={this.props.onClick}>
-        Sign In
+        {this.state.buttonText}
         {/* <AccountCircleIcon /> */}
       </Button>
     )

d2b6ed3656d16301edef16626e8a172f0f5c050a
Denny Wiseman <d.wiseman@utoronto.ca>
2019-03-01 00:12:46 -0500

    changed the hardcoded recs

--- src/Fixtures.js
+++ src/Fixtures.js
@@ -1,5 +1,12 @@
 import ago from './images/ago.jpg'
 import band from './images/pkew.jpg'
+import northropfryehall from './images/northropfryehall.jpg'
+import cntower from './images/cntower.jpg'
+import bahen from './images/bahen.jpg'
+import ripleysaquarium from './images/ripleysaquarium.jpg'
+import steves from './images/steves.jpg'
+import hashimoto from './images/hashimoto.jpg'
+
 // this is a js file containing all of the hardcoded data which will come from the server in phase 2
 const privileges = {
   admin: 1,
@@ -29,7 +36,7 @@ const recommendations = [
     title: 'Art Gallery of Ontario',
     category: 'Arts & Entertainment',
     subcategory: 'Art Galleries',
-    address: '317 Dundas St W',
+    address: '317 Dundas St West',
     opens: '0900',
     closes: '2000',
     price: 2,
@@ -39,71 +46,71 @@ const recommendations = [
     title: 'PKEW @Horseshoe Tavern',
     category: 'Events',
     subcategory: 'Other Events',
-    address: '370 Queen St W',
+    address: '370 Queen St West',
     opens: '2000',
     closes: '0100',
     price: 1,
     image: band
   },
   {
-    title: 'Art Gallery of Ontario',
+    title: 'Bahen Centre for Information Technology',
+    category: 'Colleges & Universities',
+    subcategory: 'College Academic Buildings',
+    address: '40 St George St',
+    opens: '0000',
+    closes: '0000',
+    price: 1,
+    image: bahen
+  },
+  {
+    title: 'Northrop Frye McDonalds',
+    category: 'Colleges & Universities',
+    subcategory: 'College Academic Buildings',
+    address: 'Building 515, 73 Queens Park Crescent East',
+    opens: '0000',
+    closes: '0000',
+    price: 1,
+    image: northropfryehall
+  },
+  {
+    title: 'CN Tower',
+    category: 'Professional & Other Places',
+    subcategory: 'Building',
+    address: '301 Front St West',
+    opens: '0900',
+    closes: '2230',
+    price: 3,
+    image: cntower
+  },
+  {
+    title: 'Ripleys Aquarium of Canada',
     category: 'Arts & Entertainment',
-    subcategory: 'Art Galleries',
-    address: '317 Dundas St W',
+    subcategory: 'Aquarium',
+    address: '288 Bremner Blvd',
     opens: '0900',
-    closes: '2000',
-    price: 2,
-    image: ago
+    closes: '2300',
+    price: 3,
+    image: ripleysaquarium
   },
   {
-    title: 'PKEW @Horseshoe Tavern',
-    category: 'Events',
-    subcategory: 'Other Events',
-    address: '370 Queen St W',
-    opens: '2000',
-    closes: '0100',
-    price: 1,
-    image: band
+    title: 'Hashimoto Restaurant',
+    category: 'Food',
+    subcategory: 'Japanese Restaurant',
+    address: '6 Garamond Ct',
+    opens: '1800',
+    closes: '2300',
+    price: 5,
+    image: hashimoto
   },
   {
-    title: 'Art Gallery of Ontario',
-    category: 'Arts & Entertainment',
-    subcategory: 'Art Galleries',
-    address: '317 Dundas St W',
-    opens: '0900',
-    closes: '2000',
-    price: 2,
-    image: ago
-  },
-  {
-    title: 'PKEW @Horseshoe Tavern',
-    category: 'Events',
-    subcategory: 'Other Events',
-    address: '370 Queen St W',
-    opens: '2000',
-    closes: '0100',
-    price: 1,
-    image: band
-  },
-  {
-    title: 'Art Gallery of Ontario',
-    category: 'Arts & Entertainment',
-    subcategory: 'Art Galleries',
-    address: '317 Dundas St W',
-    opens: '0900',
-    closes: '2000',
-    price: 2,
-    image: ago
-  },
-  {
-    title: 'PKEW @Horseshoe Tavern',
-    category: 'Events',
-    subcategory: 'Other Events',
-    address: '370 Queen St W',
-    opens: '2000',
-    closes: '0100',
-    price: 1,
-    image: band
+    title: 'Steves Music Store',
+    category: 'Shops & Services',
+    subcategory: 'Music Stores',
+    address: '415 Queen St West',
+    opens: '1000',
+    closes: '2100',
+    price: 3,
+    image: steves
   }
 ]
 
